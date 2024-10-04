11e163f30712f6dca25f6c72265f83fd7881fc51
Nicholas Fite <nicholasfite@Nicholass-MacBook-Pro.local>
2024-10-03 23:19:36 -0500

    fixing error page

--- src/components/error-page.tsx
+++ src/components/error-page.tsx
@@ -1,15 +1,19 @@
 import { useRouteError } from "react-router-dom";
 import SideBar from "../Sidebar";
+import "../Page.css"
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <SideBar/>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+    <div className="Page">
+        <SideBar/>
+      <div className="Content">
+
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+      </div>
     </div>
   );
 }
